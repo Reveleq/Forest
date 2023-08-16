ec6145a3ba6ee7a4e5ebbd4ba26465f5a4f264ba
Reveleq <damianw1820@gmail.com>
2023-08-16 18:04:24 +0200

    added fixed nav and create contact.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,26 @@
+document.addEventListener('DOMContentLoaded', () =>{
 const burgerBtn = document.querySelector('.mobile-nav__button')
+const allNavItems = document.querySelectorAll('.mobile-nav__link')
 const navLinks = document.querySelector('.mobile-nav__links')
+const heroPlace = document.querySelector('.hero-place')
+const mobileNav = document.querySelector('.mobile-nav')
 const showLinks = () => {
- 
- if(navLinks.classList.contains('animation-show')){
-    navLinks.classList.remove('animation-show')
-    navLinks.classList.add('animation-hide')
-     setTimeout( () => navLinks.classList.toggle('active'), 500)
- }else{
-    navLinks.classList.remove('animation-hide')
-    navLinks.classList.add('animation-show') 
-    navLinks.classList.toggle('active')
- }
-
-
+   navLinks.classList.toggle('active')
+   heroPlace.classList.toggle('active-hero')
 }
+allNavItems.forEach(e => e.addEventListener('click', () => {
+   navLinks.classList.remove('active')
+   heroPlace.classList.remove('active-hero')
+}))
+const addShadow = () => {
+   if(window.scrollY >= 350){
+   mobileNav.classList.add('shadow')
+   navLinks.classList.add('shadow')
+   }else{
+      mobileNav.classList.remove('shadow')
+   navLinks.classList.remove('shadow')
+   }
+}
+window.addEventListener('scroll', addShadow)
 burgerBtn.addEventListener('click', showLinks)
+})
