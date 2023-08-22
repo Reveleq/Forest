fbd150d1d1f4bb09f500ecc1dc940ec930ff1e3b
Damian W <reveleq@MacBook-Air-Damian.local>
2023-08-22 20:45:10 +0200

    change style position

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
 	const burgerBtn = document.querySelector(".mobile-nav__button");
 	const allNavItems = document.querySelectorAll(".mobile-nav__link");
 	const navLinks = document.querySelector(".mobile-nav__links");
+	const secondNavLinks = document.querySelector('.nav__links')
 	const heroPlace = document.querySelector(".hero-place");
 	const mobileNav = document.querySelector(".mobile-nav");
 	const nav = document.querySelector(".nav");
 	const acutalYearSpan = document.querySelector('.forest__year-actual')
 	const year = new Date().getFullYear()
-	const Sections = document.querySelectorAll('section')
+	const sections = document.querySelectorAll('section')
 	acutalYearSpan.textContent = year
 
 	const checkNav = () => {
